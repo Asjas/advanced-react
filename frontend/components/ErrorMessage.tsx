aa5a94daf4d89117639206691f15736de7016f05
A-J Roos <asjas@hey.com>
2021-02-07 13:17:34 +0200

    update ErrorMessage component

--- frontend/components/ErrorMessage.tsx
+++ frontend/components/ErrorMessage.tsx
@@ -29,21 +29,7 @@ type DisplayErrorProps = {
 
 const DisplayError = ({ error }: DisplayErrorProps) => {
   if (!error || !error.message) return null;
-  if (
-    error instanceof ApolloError &&
-    error.networkError &&
-    error.networkError.result &&
-    error.networkError.result.errors.length
-  ) {
-    return error.networkError.result.errors.map((error, i) => (
-      <ErrorStyles key={i}>
-        <p data-test="graphql-error">
-          <strong>Shoot!</strong>
-          {error.message.replace('GraphQL error: ', '')}
-        </p>
-      </ErrorStyles>
-    ));
-  }
+
   return (
     <ErrorStyles>
       <p data-test="graphql-error">
