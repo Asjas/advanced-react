3f9cdbdfaba2b62a33cf1b1d0e7e86b51b5de7b6
A-J Roos <asjas@hey.com>
2021-02-08 10:07:02 +0200

    run prettier on file

--- backend/mutations/addToCart.ts
+++ backend/mutations/addToCart.ts
@@ -1,47 +1,53 @@
-
 import { KeystoneContext } from '@keystone-next/types';
-import { Session } from './../types';
-import { CartItemCreateInput, CartItemsUpdateInput } from './../.keystone/schema-types';
+import { Session } from '../types';
+import {
+  CartItemCreateInput,
+  CartItemsUpdateInput,
+} from '../.keystone/schema-types';
 
-async function addToCart(root: any, { productId }: {productId: string}, context: KeystoneContext): Promise<CartItemCreateInput> {
-  const { session }: {session?: Session} = context;
+async function addToCart(
+  root: any,
+  { productId }: { productId: string },
+  context: KeystoneContext
+): Promise<CartItemCreateInput> {
+  const { session }: { session?: Session } = context;
 
-  if(!session.itemId) {
-    throw new Error('You must be logged in to do this!')
+  if (!session.itemId) {
+    throw new Error('You must be logged in to do this!');
   }
 
-  type AllCartItems = CartItemCreateInput[] & CartItemsUpdateInput[]
+  type AllCartItems = CartItemCreateInput[] & CartItemsUpdateInput[];
 
-  const allCartItems = await context.lists.CartItem.findMany({
+  const allCartItems = (await context.lists.CartItem.findMany({
     where: {
       user: {
         id: session.itemId,
       },
       product: {
         id: productId,
-      }
+      },
     },
     resolveFields: 'id,quantity',
-  }) as AllCartItems;
+  })) as AllCartItems;
 
   const [existingCartItem] = allCartItems;
 
-  if(existingCartItem) {
-    return await context.lists.CartItem.updateOne({
+  if (existingCartItem) {
+    return context.lists.CartItem.updateOne({
       id: existingCartItem.id,
       data: { quantity: existingCartItem.quantity + 1 },
-    })
+    });
   }
 
-  return await context.lists.CartItem.createOne({
+  return context.lists.CartItem.createOne({
     data: {
       product: {
-        connect: {id: productId},
+        connect: { id: productId },
       },
       user: {
         connect: { id: session.itemId },
       },
-    }
+    },
   });
 }
 
