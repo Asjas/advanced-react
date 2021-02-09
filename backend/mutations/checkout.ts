73fb888721cbd2fe3bd099772ffd5d81ff7be754
A-J Roos <asjas@hey.com>
2021-02-09 21:32:47 +0200

    finish course up to video #55

---
+++ backend/mutations/checkout.ts
@@ -0,0 +1,65 @@
+import { KeystoneContext } from '@keystone-next/types';
+import {
+  CartItemCreateInput,
+  OrderCreateInput,
+} from '../.keystone/schema-types';
+import stripeConfig from '../lib/stripe';
+
+const gql = String.raw;
+
+async function checkout(
+  _root: unknown,
+  { token }: { token: string },
+  context: KeystoneContext
+): Promise<OrderCreateInput> {
+  const userId = context.session.itemId;
+
+  if (!userId) {
+    throw new Error('Sorry! You must be signed in to create an order!');
+  }
+
+  const user = await context.lists.User.findOne({
+    where: { id: userId },
+    resolveFields: gql`
+      id
+      name
+      email
+      cart {
+        id
+        quantity
+        product
+        description
+        photo {
+          id
+          image {
+            id
+            publicUrlTransformed
+          }
+        }
+      }
+    `,
+  });
+
+  const cartItems = user.cart.filter((cartItem) => cartItem.product);
+  const amount = cartItems.reduce(
+    (tally: number, cartItem) =>
+      tally + cartItem.quantity * cartItem.product.price,
+    0
+  );
+
+  const charge = await stripeConfig.paymentIntents
+    .create({
+      amount,
+      currency: 'USD',
+      confirm: true,
+      payment_method: token,
+    })
+    .catch((err) => {
+      console.error(err);
+      throw new Error(err);
+    });
+
+  console.log({ charge });
+}
+
+export default checkout;
--- backend/mutations/index.ts
+++ backend/mutations/index.ts
@@ -1,16 +1,19 @@
-import { graphQLSchemaExtension } from "@keystone-next/keystone/schema";
+import { graphQLSchemaExtension } from '@keystone-next/keystone/schema';
 import addToCart from './addToCart';
+import checkout from './checkout';
 
 const gql = String.raw;
 export const extendGraphqlSchema = graphQLSchemaExtension({
   typeDefs: gql`
     type Mutation {
       addToCart(productId: ID): CartItem
+      checkout(token: String!): Order
     }
   `,
   resolvers: {
     Mutation: {
       addToCart,
-    }
-  }
-})
+      checkout,
+    },
+  },
+});
