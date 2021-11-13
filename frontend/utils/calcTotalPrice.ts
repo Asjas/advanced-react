b5d290e0ccfb07669f0ace44a5f68e471f0ff74f
A-J Roos <asjas@hey.com>
2021-11-13 08:45:33 +0200

    fix calctotalprice with typescript strict mode

--- frontend/utils/calcTotalPrice.ts
+++ frontend/utils/calcTotalPrice.ts
@@ -1,8 +1,9 @@
-import { CartItem } from '../types/generated-queries';
+import { CartItem } from "../types/generated-queries";
 
 export default function calcTotalPrice(cart: CartItem[]) {
   return cart.reduce((tally, cartItem) => {
-    if (!cartItem.product) return tally;
+    if (!cartItem.quantity || !cartItem.product || !cartItem.product.price) return tally;
+
     return tally + cartItem.quantity * cartItem.product.price;
   }, 0);
 }
